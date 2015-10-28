f7e88f20084fd24d7ec5c724b187b83959a62148
MateuszMateo <k.mateusz120@gmail.com>
2015-10-28 22:03:16 +0100

    Działający kod

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+long long int wartosciTrojkata[3000][3000];
+
+long long Symbol(){
+    for(int N=0;N < 2999; ++N) {
+        wartosciTrojkata[N][0] = 1;
+        wartosciTrojkata[N][N] = 1;
+      for(int K=1; K <= N-1; ++K) {
+        wartosciTrojkata[N][K] = wartosciTrojkata[N-1][K-1] + wartosciTrojkata[N-1][K];
+
+
+    }
+
+
+}
+}
+
+int main()
+{
+    cout << "Podaj wartosci" << endl;
+    Symbol();
+    long long A,B,C;
+    cin >> A >> B >> C;
+    cout << wartosciTrojkata[A+B+C][A]*wartosciTrojkata[B+C][B]<<endl;
+    return 0;
+}
+
